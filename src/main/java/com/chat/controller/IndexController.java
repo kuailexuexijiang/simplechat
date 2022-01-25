d2fcd0fa1a4e15fe6c4b10c0a27de0e6ddebc5c4
张新 <zhang_xin@tom.com>
2022-01-25 15:28:26 +0800

    动态ip

--- src/main/java/com/chat/controller/IndexController.java
+++ src/main/java/com/chat/controller/IndexController.java
@@ -1,17 +1,27 @@
 package com.chat.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.InetAddress;
 
 /**
  * 页面跳转控制器类
  * @author zhangxin
  */
 @Controller
+@Slf4j
 public class IndexController {
 
     @GetMapping(value = "/index")
-    public String index(){
+    public String index(ModelMap modelMap){
+        try {
+            modelMap.put("ip", InetAddress.getLocalHost().getHostAddress());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
         return "index";
     }
 }
